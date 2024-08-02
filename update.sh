5acdc6ae66bc246e2a52a500b83c17c76a15efb2
Aetherinox <aetherinox@proton.me>
2024-08-02 14:49:02 -0700

    chore: update update.sh

--- update.sh
+++ update.sh
@@ -192,6 +192,16 @@ if ! [ -x "$(command -v lastversion)" ]; then
     # #
 
     pip install lastversion --break-system-packages
+    sudo touch /etc/profile.d/lastversion.sh
+
+    envpath_add_lastversion '${HOME}/bin'
+
+    echo 'export PATH="${HOME}/bin:$PATH"' | sudo tee /etc/profile.d/lastversion.sh
+
+    . ~/.bashrc
+    . ~/.profile
+
+    source ${HOME}/.profile # not executing for some reason
 fi
 
 # #
